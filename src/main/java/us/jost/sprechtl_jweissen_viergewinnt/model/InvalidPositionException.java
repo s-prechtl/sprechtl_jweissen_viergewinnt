ce5a2ddd0c6ee5d909ecb589e7552f1d5c2ddd91
s-prechtl <stefan@tague.at>
2022-01-28 09:36:09 +0100

    Dateikommentare geändert, Methodenkommentare nachgetragen/verändert

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/model/InvalidPositionException.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/model/InvalidPositionException.java
@@ -6,16 +6,19 @@ package us.jost.sprechtl_jweissen_viergewinnt.model;
  *           Fachrichtung Informationstechnologie und Netzwerktechnik
  *----------------------------------------------------------------------------*/
 /**
- * Kurzbeschreibung
+ * Position nicht verfügbar.
  *
  * @author  : Stefan Prechtl
  * @date    : 28.01.2022
  *
  * @details
- *   Falls angegebene Position nicht im Feld liegt, wird diese Exception geworfen.
+ *   Falls angegebene Position nicht im Feld liegt, oder die gewünschte Reihe bereits voll ist, wird diese Exception geworfen.
  *
  */
 public class InvalidPositionException extends Exception{
+    /**
+     * Konstruktor
+     */
     public InvalidPositionException() {
         super("This postion isn't valid!");
     }
