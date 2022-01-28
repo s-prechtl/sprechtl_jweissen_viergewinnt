18f1f33888f28d2edd5ea257870c0ab9032346f6
j-weissen <jweissen@htl-steyr.ac.at>
2022-01-28 02:03:07 +0100

    ControllerConsole implementiert

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerConsole.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerConsole.java
@@ -1,9 +1,15 @@
 package us.jost.sprechtl_jweissen_viergewinnt.controller;
 
 import us.jost.sprechtl_jweissen_viergewinnt.model.Game;
-import us.jost.sprechtl_jweissen_viergewinnt.view.BoardView;
-import us.jost.sprechtl_jweissen_viergewinnt.view.MessageView;
-import us.jost.sprechtl_jweissen_viergewinnt.view.PromptView;
+import us.jost.sprechtl_jweissen_viergewinnt.model.InvalidPositionException;
+import us.jost.sprechtl_jweissen_viergewinnt.model.PlayerID;
+import us.jost.sprechtl_jweissen_viergewinnt.model.UndoNotPossibleException;
+import us.jost.sprechtl_jweissen_viergewinnt.view.*;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ControllerConsole {
     private static Game game;
@@ -11,11 +17,83 @@ public class ControllerConsole {
     private static PromptView promptView;
     private static MessageView messageView;
 
-    public static void main(String[] args) {
+    private static Scanner scanner;
 
+    public static void main(String[] args) {
+        initialize();
+
+        char action;
+        String input;
+        boolean win, tie;
+        Pattern legalAction = Pattern.compile("[0-6]|[RU]", Pattern.CASE_INSENSITIVE);
+
+        do {
+            boardView.display(game.getPlayerIDBoard());
+            do {
+                promptView.display(game.getPlayerName(game.getCurrPlayer()), "Enter your action: (0-6, [R]eset, [U]ndo)");
+                input = scanner.nextLine();
+            } while (!legalAction.matcher(input).matches());
+            action = input.toUpperCase().toCharArray()[0];
+
+            if (Character.isDigit(action)) {
+                try {
+                    game.play(Integer.parseInt(String.valueOf(action)));
+                    game.switchCurrPlayer();
+                } catch (InvalidPositionException ipe) {
+                    messageView.display(ipe.getMessage());
+                }
+            } else if (action == 'U') {
+                try {
+                    game.undo();
+                    messageView.display("Last move undone!");
+                } catch (UndoNotPossibleException unpe) {
+                    messageView.display(unpe.getMessage());
+                }
+            } else if (action == 'R') {
+                game.reset();
+            }
+            win = game.checkWin();
+            tie = game.checkTie();
+        } while (!(win || tie));
+
+        boardView.display(game.getPlayerIDBoard());
+        if (win) {
+            game.switchCurrPlayer();
+            messageView.display(game.getPlayerName(game.getCurrPlayer()) + " has won! Congratulations!");
+        } else {
+            messageView.display("It's a tie!");
+        }
     }
 
-    private static void init() {
+    private static void initialize() {
+        scanner = new Scanner(System.in);
+        promptView = new PromptViewConsole();
+        messageView = new MessageViewConsole();
 
+        HashMap<PlayerID, Character> defaultPlayingChars = new HashMap<>();
+        defaultPlayingChars.put(PlayerID.Player0, 'X');
+        defaultPlayingChars.put(PlayerID.Player1, 'O');
+
+        String name1, name2;
+        promptView.display("Player0", "Enter your name:");
+        name1 = scanner.nextLine();
+        do {
+            promptView.display("Player1", "Enter your name:");
+            name2 = scanner.nextLine();
+        } while (Objects.equals(name1, name2));
+        game = new Game(name1, name2);
+
+        HashMap<PlayerID, Character> playingChars = new HashMap<>();
+        for (PlayerID playerID : defaultPlayingChars.keySet()) {
+            promptView.display(game.getPlayerName(playerID), "Enter your playing character: (Enter for default)");
+            String temp = scanner.nextLine();
+            if (!Objects.equals(temp, "")) {
+                playingChars.put(playerID, temp.toCharArray()[0]);
+            } else {
+                playingChars.put(playerID, defaultPlayingChars.get(playerID));
+            }
+        }
+        boardView = new BoardViewConsole(playingChars);
+        messageView.display("Let the game begin!");
     }
 }
