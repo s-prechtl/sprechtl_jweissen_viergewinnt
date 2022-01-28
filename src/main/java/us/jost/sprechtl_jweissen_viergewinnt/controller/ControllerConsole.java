f164551a23478c1c9e4b4602925dfe2e290824f3
j-weissen <jweissen@htl-steyr.ac.at>
2022-01-28 09:41:38 +0100

    Code Refactoring, Kommentare hinzugefügt

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerConsole.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerConsole.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+//TODO j-weissen: Klassendiagramm updaten!!!
 public class ControllerConsole {
     private static Game game;
     private static BoardView boardView;
@@ -18,23 +19,17 @@ public class ControllerConsole {
     private static MessageView messageView;
 
     private static Scanner scanner;
+    private static Pattern legalAction;
 
     public static void main(String[] args) {
         initialize();
 
         char action;
-        String input;
-        boolean win, tie, restart;
-        Pattern legalAction = Pattern.compile("[0-6]|[RU]", Pattern.CASE_INSENSITIVE);
+        boolean win, tie, reset;
+
         do {
             do {
-                boardView.display(game.getPlayerIDBoard());
-                do {
-                    promptView.display(game.getPlayerName(game.getCurrPlayer()), "Enter your action: (0-6, [R]eset, [U]ndo)");
-                    input = scanner.nextLine();
-                } while (!legalAction.matcher(input).matches());
-                action = input.toUpperCase().toCharArray()[0];
-
+                action = promptAction();
                 if (Character.isDigit(action)) {
                     try {
                         game.play(Integer.parseInt(String.valueOf(action)));
@@ -56,32 +51,32 @@ public class ControllerConsole {
                 tie = game.checkTie();
             } while (!(win || tie));
 
-            boardView.display(game.getPlayerIDBoard());
-            if (win) {
-                game.switchCurrPlayer();
-                messageView.display(game.getPlayerName(game.getCurrPlayer()) + " has won! Congratulations!");
-            } else {
-                messageView.display("It's a tie!");
-            }
+            showResult(win);
 
-            messageView.display("Do you want to play again? (y/N)");
-            input = scanner.nextLine();
-            restart = Objects.equals(input, "y");
-            if (restart) {
+            reset = promptReset();
+            if (reset) {
                 game.reset();
             }
-        } while (restart);
+        } while (reset);
     }
 
+    /**
+     * initialisiert die Attribute der Controller-Klasse
+     */
     private static void initialize() {
         scanner = new Scanner(System.in);
         promptView = new PromptViewConsole();
         messageView = new MessageViewConsole();
+        initializeGame();
+        boardView = new BoardViewConsole(promptPlayingChars());
+        legalAction = Pattern.compile("[0-6]|[RU]", Pattern.CASE_INSENSITIVE);
+        messageView.display("Let the game begin!");
+    }
 
-        HashMap<PlayerID, Character> defaultPlayingChars = new HashMap<>();
-        defaultPlayingChars.put(PlayerID.Player0, 'X');
-        defaultPlayingChars.put(PlayerID.Player1, 'O');
-
+    /**
+     * initialisiert die Game-Klasse, fragt dafür die Spieler nach ihren Namen
+     */
+    private static void initializeGame() {
         String name1, name2;
         promptView.display("Player0", "Enter your name:");
         name1 = scanner.nextLine();
@@ -90,6 +85,19 @@ public class ControllerConsole {
             name2 = scanner.nextLine();
         } while (Objects.equals(name1, name2));
         game = new Game(name1, name2);
+    }
+
+    /**
+     * Fragt die Spieler nach ihren Spielsymbolen
+     * Wird keine Eingabe getätigt, werden die Standartwerte X und O verwendet
+     *
+     * @return HashMap der Spieler und ihrer Symbole
+     *         kann so an den BoardViewConsole-Konstruktor übergeben werden
+     */
+    private static HashMap<PlayerID, Character> promptPlayingChars() {
+        HashMap<PlayerID, Character> defaultPlayingChars = new HashMap<>();
+        defaultPlayingChars.put(PlayerID.Player0, 'X');
+        defaultPlayingChars.put(PlayerID.Player1, 'O');
 
         HashMap<PlayerID, Character> playingChars = new HashMap<>();
         do {
@@ -104,7 +112,48 @@ public class ControllerConsole {
             }
         } while (playingChars.get(PlayerID.Player0) == playingChars.get(PlayerID.Player1));
 
-        boardView = new BoardViewConsole(playingChars);
-        messageView.display("Let the game begin!");
+        return defaultPlayingChars;
+    }
+
+    /**
+     * Fragt den currPlayer nach einem Zug / einer Aktion
+     * sollte die Eingabe ungültig sein (= nicht auf das legalAction Pattern passen), wird erneut gefragt
+     *
+     * @return gibt den eingegeben gültigen char zurück
+     */
+    private static char promptAction() {
+        String input;
+        boardView.display(game.getPlayerIDBoard());
+        do {
+            promptView.display(game.getPlayerName(game.getCurrPlayer()), "Enter your action: (0-6, [R]eset, [U]ndo)");
+            input = scanner.nextLine();
+        } while (!legalAction.matcher(input).matches());
+        return input.toUpperCase().toCharArray()[0];
+    }
+
+    /**
+     * Gibt das Ergebnis des Spieles aus
+     * @param win gibt an, ob jemand gewonnen hat oder ob es unentschieden ausging
+     */
+    private static void showResult(boolean win) {
+        boardView.display(game.getPlayerIDBoard());
+        if (win) {
+            game.switchCurrPlayer();
+            messageView.display(game.getPlayerName(game.getCurrPlayer()) + " has won! Congratulations!");
+        } else {
+            messageView.display("It's a tie!");
+        }
+    }
+
+    /**
+     * fragt nach dem Spielende, ob das spiel neu gestartet werden soll
+     * @return ob das spiel neu gestartet werden soll
+     */
+    private static boolean promptReset() {
+        String input;
+        messageView.display("Do you want to play again? (y/N)");
+        input = scanner.nextLine();
+        return Objects.equals(input, "y");
     }
 }
+
