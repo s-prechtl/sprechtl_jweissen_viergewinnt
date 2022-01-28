39ab0983c427347597f3f09fd6edda09a0da8314
j-weissen <jweissen@htl-steyr.ac.at>
2022-01-28 02:05:57 +0100

    winCount zwischen versch. Richtungen zurückgesetzt

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/model/Board.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/model/Board.java
@@ -27,69 +27,69 @@ public class Board {
     }
 
     public boolean checkWin() {
-        int winCount = 0;
-        PlayerID playerID = previouslyChanged.getState();
-        int x = previouslyChanged.getX();
-        int y = previouslyChanged.getY();
-        int ix;
-        int iy;
-        boolean isWin;
+        if (previouslyChanged != null) {
+            int winCount = 0;
+            PlayerID playerID = previouslyChanged.getState();
+            int x = previouslyChanged.getX();
+            int y = previouslyChanged.getY();
+            int ix;
+            int iy;
+            boolean isWin = false;
 
-        //horizontal and vertical
-        isWin = isWinHorVer(x, y, playerID) || isWinHorVer(y, x, playerID);
+            //horizontal
+            for (ix = initI_negative(x); inArea(ix, y) && !isWin; ix++) {
+                if (accessCell(ix, y).getState() == playerID) {
+                    winCount++;
+                } else {
+                    winCount = 0;
+                }
+                if (winCount >= WINDIST) {
+                    isWin = true;
+                }
+            }
 
-        //left up to right down
-        for (ix = initI_negative(x), iy = initI_negative(y); inArea(ix, iy) && !isWin; ix++, iy++) {
-            try {
+            //vertical
+            winCount = 0;
+            for (iy = initI_negative(y); inArea(x, iy) && !isWin; iy++) {
+                if (accessCell(x, iy).getState() == playerID) {
+                    winCount++;
+                } else {
+                    winCount = 0;
+                }
+                if (winCount >= WINDIST) {
+                    isWin = true;
+                }
+            }
+
+            //left up to right down
+            winCount = 0;
+            for (ix = initI_negative(x), iy = initI_negative(y); inArea(ix, iy) && !isWin; ix++, iy++) {
                 if (accessCell(ix, iy).getState() == playerID) {
                     winCount++;
                 } else {
                     winCount = 0;
                 }
-            } catch (InvalidPositionException ignored) {
+                if (winCount >= WINDIST) {
+                    isWin = true;
+                }
             }
-            if (winCount >= WINDIST) {
-                isWin = true;
-            }
-        }
 
-        //left down to right up
-        for (ix = initI_negative(x), iy = initI_positive(y); inArea(ix, iy) && !isWin; ix++, iy--) {
-            try {
+            //right up to left down
+            winCount = 0;
+            for (ix = initI_negative(x), iy = initI_positive(y); inArea(ix, iy) && !isWin; ix++, iy--) {
                 if (accessCell(ix, iy).getState() == playerID) {
                     winCount++;
                 } else {
                     winCount = 0;
                 }
-            } catch (InvalidPositionException ignored) {
-            }
-            if (winCount >= WINDIST) {
-                isWin = true;
-            }
-        }
-
-        return isWin;
-    }
-
-    private boolean isWinHorVer(int direction, int i, PlayerID playerID) {
-        boolean isWin = false;
-        int winCount = 0;
-
-        for (direction = initI_negative(direction); inArea(direction, 0) && !isWin; direction++) {
-            try {
-                if (accessCell(direction, i).getState() == playerID) {
-                    winCount++;
-                } else {
-                    winCount = 0;
+                if (winCount >= WINDIST) {
+                    isWin = true;
                 }
-            } catch (InvalidPositionException ignored) {
             }
-            if (winCount >= WINDIST) {
-                isWin = true;
-            }
-        }
 
-        return isWin;
+            return isWin;
+        }
+        return false;
     }
 
     /**
@@ -100,22 +100,22 @@ public class Board {
     }
 
     /**
-     * @return Maximum value to end of area/to the winning number 4/WINDIST from current location
+     * @return Maximum value to end of area top/to the winning number 4/WINDIST from current location
      */
     private int initI_positive(int z) {
-        return (z + WINDIST >= COLS) ? COLS - 1 : z + WINDIST;
+        return (z + WINDIST >= ROWS) ? ROWS - 1 : z + WINDIST;
     }
+
 
     public boolean checkTie() {
         boolean isTie = true;
         for (int x = 0; x < COLS && isTie; x++) {
             for (int y = 0; y < ROWS && isTie; y++) {
-                try {
-                    if (accessCell(x, y).getState() == null) {
-                        isTie = false;
-                    }
-                } catch (InvalidPositionException ignored) {
+
+                if (accessCell(x, y).getState() == null) {
+                    isTie = false;
                 }
+
             }
         }
         return isTie;
@@ -130,10 +130,7 @@ public class Board {
         for (int x = 0; x < COLS; x++) {
             boardIDs.add(new ArrayList<>());
             for (int y = 0; y < ROWS; y++) {
-                try {
-                    boardIDs.get(x).add(accessCell(x, y).getState());
-                } catch (InvalidPositionException ignored) {
-                }
+                boardIDs.get(x).add(accessCell(x, y).getState());
             }
         }
 
@@ -142,7 +139,7 @@ public class Board {
 
     /**
      * @return next free spot.
-     * @throws IllegalArgumentException If row is already full.
+     * @throws InvalidPositionException If row is already full.
      */
     public int getNextFreeRow(int col) throws InvalidPositionException {
         int nextFreeRow = -1;
@@ -158,11 +155,7 @@ public class Board {
         return nextFreeRow;
     }
 
-    public Cell accessCell(int x, int y) throws InvalidPositionException {
-        if (!inArea(x, y)) {
-            throw new InvalidPositionException();
-        }
-
+    public Cell accessCell(int x, int y) {
         return board.get(x).get(y);
     }
 
