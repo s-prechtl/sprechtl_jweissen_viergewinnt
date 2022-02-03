896aeb686f07f3a01be5c918de0825ce9a5b6a05
s-prechtl <sprechtl@htl-steyr.ac.at>
2022-02-03 15:07:08 +0100

    added style.css for FXML, vierGewinnt-view.fxml angefertigt

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerGUI.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/controller/ControllerGUI.java
@@ -1,14 +1,21 @@
 package us.jost.sprechtl_jweissen_viergewinnt.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ControllerGUI {
     @FXML
-    private Label welcomeText;
+    private Label LabelMessage;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onButtonUndoClicked() {
+    }
+
+    public void onButtonResetClicked() {
+    }
+
+    public void onButtonQuitClicked() {
+        Platform.exit();
     }
 }
