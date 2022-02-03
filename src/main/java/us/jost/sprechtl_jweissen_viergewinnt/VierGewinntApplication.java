f97a9d2da98512cdc19e4574723f243ecd50ab32
s-prechtl <sprechtl@htl-steyr.ac.at>
2022-02-03 16:24:32 +0100

    erste Tests der neuen FXML-Dateien

--- src/main/java/us/jost/sprechtl_jweissen_viergewinnt/VierGewinntApplication.java
+++ src/main/java/us/jost/sprechtl_jweissen_viergewinnt/VierGewinntApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import us.jost.sprechtl_jweissen_viergewinnt.controller.ControllerColorPickerGUI;
+import us.jost.sprechtl_jweissen_viergewinnt.controller.ControllerNamePickerGUI;
 
 import java.io.IOException;
 
@@ -15,6 +17,8 @@ public class VierGewinntApplication extends Application {
         stage.setTitle("VIER GEWINNT!");
         stage.setScene(scene);
         stage.show();
+        ControllerColorPickerGUI.display();
+        ControllerNamePickerGUI.display();
     }
 
     public static void main(String[] args) {
